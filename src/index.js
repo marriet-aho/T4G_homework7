f6e4946b088a21712191dd8d055c48eaf8d2fc7a
marriet-aho <marrietaho2020@gmail.com>
2024-11-29 14:37:16 +0000

    adding all files

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ let email = prompt("write your email?");
 
 if (age < 12) {
     alert(`Hi ${name}, you are ${age} years old and you are too young to register. Sorry 😔😔`);
-} else if ( 12  > age < 18) {
+} else if ( 12  < age < 18) {
     alert(`Hi ${name}, you are ${age} years old and you have limited options to register for. We will keep in touch via your email: ${email} 👋🏾👋🏾.`);
 } else if (age > 18) {
     alert(`Hi ${name}, you are ${age} years old and you can register for any option of your choosing. We will keep in touch via your email: ${email} 👋🏾`);
@@ -13,19 +13,5 @@ if (age < 12) {
     alert("Please enter a valid age.");
 }
 
-
-// Function to prompt the user for their details and display appropriate alerts
-// function registerUser() {
-//     // Prompt for user input
-//     const name = prompt("Enter your name:");
-//     const age = parseInt(prompt("Enter your age:"));
-//     const email = prompt("Enter your email:");
-
-//     // Check if the inputs are valid
-//     if (!name || isNaN(age) || !email) {
-//         alert("Please provide valid inputs for name, age, and email.");
-//         return;
-//     }
-// }
 
 
